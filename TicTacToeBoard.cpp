592d42c9afa5fedf97d779789febf0d7a984482d
yyang31 <yyang2@mail.csuchico.edu>
2019-11-20 21:00:04 -0800

    fixes logical comparison error

--- TicTacToeBoard.cpp
+++ TicTacToeBoard.cpp
@@ -73,24 +73,24 @@ Piece TicTacToeBoard::getWinner()
   for (int i = 0; i < BOARDSIZE; i++)
   {
     // check horzontial win conditions
-    if (board[i][0] == board[i][1] == board[i][2])
+    if (board[i][0] == board[i][1] && board[i][1] == board[i][2])
     {
       return board[i][0];
     }
 
     // check vertical win conditions
-    if (board[0][i] == board[1][i] == board[2][i])
+    if (board[0][i] == board[1][i] && board[1][i] == board[2][i])
     {
       return board[0][i];
     }
   }
 
   // check diagonal win conditions
-  if (board[0][0] == board[1][1] == board[2][2])
+  if (board[0][0] == board[1][1] && board[1][1] == board[2][2])
   {
     return board[0][0];
   }
-  if (board[0][2] == board[1][1] == board[2][0])
+  if (board[0][2] == board[1][1] && board[1][1] == board[2][0])
   {
     return board[0][2];
   }
